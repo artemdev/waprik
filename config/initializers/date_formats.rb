f037eff33ed4608584d0ba42e00af18297371fac
artemdev <artem.zimovets@gmail.com>
2013-11-19 22:50:00 +0200

    Добавление папки config (без database.yml

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,3 @@
+# Custom date formats
+Time::DATE_FORMATS[:custom] = "%d %m %y"
+Time::DATE_FORMATS[:DayMonth] = "%d.%m"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waprik::Application.config.secret_token = '855d47af308c104ae5cbd550787d74a43b421462b6e42087104ae467869a4471587e72e16efc32e3b25f4842adff98a9f25c49d6144ad615b4e15e5b3b18512d'
