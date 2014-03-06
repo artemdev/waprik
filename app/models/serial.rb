6d4add605a2084e96d0b42ebd2d7d3cd25b23987
artemdev <artem.zimovets@gmail.com>
2014-03-06 23:43:50 +0200

    корректировка сортировки сериалов

--- app/models/serial.rb
+++ app/models/serial.rb
@@ -16,6 +16,6 @@ class Serial < ActiveRecord::Base
   accepts_nested_attributes_for :series
 
   scope :hits, where(hit: true)
-  scope :latest, order(series: "created_at DESC")
+  scope :latest, order("updated_at DESC")
 
 end
