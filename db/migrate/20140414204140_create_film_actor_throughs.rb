1cbccc5ceafb6aa15f6d20ed45f43e048462167f
artway <artem.zimovets@gmail.com>
2014-06-08 22:27:30 +0300

    поправлены миграции

--- db/migrate/20140414204140_create_film_actor_throughs.rb
+++ db/migrate/20140414204140_create_film_actor_throughs.rb
@@ -1,11 +1,11 @@
 class CreateFilmActorThroughs < ActiveRecord::Migration
   def change
-    create_table :films_actor_throughs do |t|
+    create_table :films_actors_through do |t|
     	t.integer :film_id
     	t.integer :actor_id
       t.timestamps
     end
-    add_index :film_actors_throughs, :film_id
-    add_index :film_actors_throughs, :actor_id
+    add_index :films_actors_through, :film_id
+    add_index :films_actors_through, :actor_id
   end
 end
