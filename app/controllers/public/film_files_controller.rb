3422fd2475995734eb4cf1ceebfea55fee8222c0
artway <artem.zimovets@gmail.com>
2014-06-09 11:46:02 +0300

    доработки файлов

--- app/controllers/public/film_files_controller.rb
+++ app/controllers/public/film_files_controller.rb
@@ -5,6 +5,6 @@ class Public::FilmFilesController < ApplicationController
 		@file = FilmFile.find(params[:id])
 		@file.film.downloads += 1
 		@file.film.save
-		send_file(@file.real_name.url)
+		send_file(@file.real_name.path)
 	end
 end
