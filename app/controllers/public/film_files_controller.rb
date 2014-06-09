8340cf3de79e2285c1af4b237f734fcb47d18df0
artway <artem.zimovets@gmail.com>
2014-06-09 10:00:09 +0300

    доработки файлов

--- app/controllers/public/film_files_controller.rb
+++ app/controllers/public/film_files_controller.rb
@@ -5,7 +5,7 @@ class Public::FilmFilesController < ApplicationController
 		if @file = FilmFile.find(params[:id])
 			@file.film.downloads += 1
 			@file.film.save
-			redirect_to(@file.real_name.url)
+			send_file(@file.real_name.path)
 		else
 			flash[:error] = "Файл не найден"
 			redirect_to :back 
