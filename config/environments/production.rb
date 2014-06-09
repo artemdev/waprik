24787ba6d7449ae90e6235da74fb4a0fb7363f3a
artway <artem.zimovets@gmail.com>
2014-06-09 12:12:27 +0300

    доработки файлов

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Waprik::Application.configure do
   config.assets.digest = true
   
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
   # config.assets.precompile += ['mobile.css']
   # Generate digests for assets URLs
   config.assets.digest = true
