d6a6b1046ea1575502f1c867dcf15210beb7f9f3
artway <artem.zimovets@gmail.com>
2014-06-09 09:39:02 +0300

    доработки файлов

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,5 @@ Waprik::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 end
