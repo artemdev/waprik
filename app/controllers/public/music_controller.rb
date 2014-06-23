644a47af5910c81f5cd424daad2170c66b9f42f6
artway <artem.zimovets@gmail.com>
2014-06-23 13:36:37 +0300

    отправка файла в музыке

--- app/controllers/public/music_controller.rb
+++ app/controllers/public/music_controller.rb
@@ -14,11 +14,11 @@ class Public::MusicController < ApplicationController
 		if @track.path.path.present?
 			case params[:bitrate]
 				when "64"
-					send_file(@track.path.path + "_64.mp3")
+					send_file "#{@track.path.path}_128.mp3", filename: @track.fname
 				when "128"
-					send_file(@track.path.path + "_128.mp3")
+					send_file "#{@track.path.path}_64.mp3", filename: @track.fname
 				when "32"
-					send_file(@track.path.path + "_32.mp3")
+					send_file "#{@track.path.path}_32.mp3", filename: @track.fname
 			end
 		end
 	end
