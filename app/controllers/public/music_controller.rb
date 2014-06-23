3816e6f62aecf62190e855b8ebd3f939fe455ee2
artway <artem.zimovets@gmail.com>
2014-06-23 09:50:33 +0300

    исправлен баг со скачиванием файла

--- app/controllers/public/music_controller.rb
+++ app/controllers/public/music_controller.rb
@@ -11,7 +11,7 @@ class Public::MusicController < ApplicationController
 
 	def download
 		@track = Mp3File.find(params[:id])
-		if @track.path.present?
+		if @track.path.path.present?
 			case params[:bitrate]
 				when "64"
 					send_file(@track.path.path + "_64.mp3")
