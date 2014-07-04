034a00e0497ea60b46efba5b209a76c203b1bd32
artway <artem.zimovets@gmail.com>
2014-07-04 16:11:03 +0300

    test dl

--- app/controllers/public/music_controller.rb
+++ app/controllers/public/music_controller.rb
@@ -29,6 +29,6 @@ class Public::MusicController < ApplicationController
 	end
 
 	def test
-		send_file "#{Rails.root}/public/Alexandra_Stan_feat._DJ_Andi_–_Set_Me_Free_64.mp3", filename: "test.mp3"
+		send_file "#{Rails.root}/public/test.mp3", filename: "test.mp3"
 	end
 end
