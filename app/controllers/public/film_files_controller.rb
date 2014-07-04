45a720642714826c63f778ac14d9a81b3b0fc74b
artway <artem.zimovets@gmail.com>
2014-07-04 21:30:47 +0300

    баг с подсчетом скачиваний фильмов

--- app/controllers/public/film_files_controller.rb
+++ app/controllers/public/film_files_controller.rb
@@ -8,14 +8,17 @@ class Public::FilmFilesController < ApplicationController
 
 	def download
 		@file = FilmFile.find(params[:film_file_id])
-		@file.film.downloads += 1
-		@file.film.save
 		redirect_to(action: "get_file", film_file_id: @file.id)
 	end
 
 	def get_file
 		@file = FilmFile.find(params[:film_file_id])
-		send_file @file.real_name.path, content_type: @file.real_name.content_type
+		if send_file @file.real_name.path, content_type: @file.real_name.content_type
+				@file.film.downloads += 1
+				@file.film.save
+		else
+			redirect_to film_file_path(@file)
+		end
 	end
 
 	def part
