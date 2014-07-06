86d4e8bca786674d1e380b8bd167fe6e4f176a70
artway <artem.zimovets@gmail.com>
2014-07-06 04:40:57 +0300

    музыкальный раздел

--- app/models/mp3_album.rb
+++ app/models/mp3_album.rb
@@ -3,4 +3,6 @@ class Mp3Album < ActiveRecord::Base
 
   has_many :tracks, class_name: 'Mp3File', foreign_key: 'album_id'
 
+  belongs_to :artist, class_name: 'Mp3Artist', foreign_key: 'artist_id'
+
 end
