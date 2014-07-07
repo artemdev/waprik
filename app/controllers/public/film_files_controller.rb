100f588d9026fc3b3e21ad40ea5b1a352554bb73
artway <artem.zimovets@gmail.com>
2014-07-07 21:44:04 +0300

    указываем размер файла при загрузке браузером

--- app/controllers/public/film_files_controller.rb
+++ app/controllers/public/film_files_controller.rb
@@ -10,16 +10,14 @@ class Public::FilmFilesController < ApplicationController
 		@file = FilmFile.find(params[:film_file_id])
 		@file.film.downloads += 1
 		@file.film.save
-		send_file @file.real_name.path, content_type: @file.real_name.content_type
-		headers['Content-Length'] = @file.size
+		send_file @file.real_name.path, content_type: @file.real_name.content_type, length: @file.size
 	end
 
 	def part
 		part = FilmPart.find(params[:film_file_id])
 		part.film.downloads += 1
 		part.film.save
-		send_file part.real_name.path, content_type: part.real_name.content_type
-		headers['Content-Length'] = part.size
+		send_file part.real_name.path, content_type: part.real_name.content_type, length: @file.size
 	end
 
 	def get_part
