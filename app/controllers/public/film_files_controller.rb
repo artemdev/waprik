1ad8afc2d98a161b752442f3aa6afff604bc946a
artway <artem.zimovets@gmail.com>
2014-07-07 19:28:29 +0300

    Скачивание фильмов

--- app/controllers/public/film_files_controller.rb
+++ app/controllers/public/film_files_controller.rb
@@ -10,7 +10,6 @@ class Public::FilmFilesController < ApplicationController
 		@file = FilmFile.find(params[:film_file_id])
 		@file.film.downloads += 1
 		@file.film.save
-		@file = FilmFile.find(params[:film_file_id])
 		send_file @file.real_name.path, content_type: @file.real_name.content_type, x_sendfile: true
 	end
 
@@ -18,8 +17,6 @@ class Public::FilmFilesController < ApplicationController
 		film_file = FilmPart.find(params[:film_file_id])
 		film_file.film.downloads += 1
 		film_file.film.save
-		redirect_to action: "get_part", film_file_id: film_file.id
-		part = FilmPart.find(params[:film_file_id])
 		send_file part.real_name.path, content_type: part.real_name.content_type, x_sendfile: true
 	end
 
