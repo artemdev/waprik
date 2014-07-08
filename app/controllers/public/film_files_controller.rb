537b59f47abd28c77084345b67c3d8a702cb0ddc
artway <artem.zimovets@gmail.com>
2014-07-08 13:57:13 +0300

    disposion inline in film_files

--- app/controllers/public/film_files_controller.rb
+++ app/controllers/public/film_files_controller.rb
@@ -10,14 +10,14 @@ class Public::FilmFilesController < ApplicationController
 		@file = FilmFile.find(params[:film_file_id])
 		@file.film.downloads += 1
 		@file.film.save
-		send_file @file.real_name.path, content_type: @file.real_name.content_type, length: @file.size
+		send_file @file.real_name.path, content_type: @file.real_name.content_type, disposition: :inline
 	end
 
 	def part
 		part = FilmPart.find(params[:film_file_id])
 		part.film.downloads += 1
 		part.film.save
-		send_file part.real_name.path, content_type: part.real_name.content_type, length: part.size
+		send_file part.real_name.path, content_type: part.real_name.content_type
 	end
 
 end
