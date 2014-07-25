4983cffa31ebb9397d4b2deb819cb12a21822703
artway <artem.zimovets@gmail.com>
2014-07-26 00:32:58 +0300

    добавление коллекции при создании трека

--- app/controllers/admin/music_controller.rb
+++ app/controllers/admin/music_controller.rb
@@ -17,7 +17,7 @@ class Admin::MusicController < ApplicationController
 		@track = Mp3File.new
 		mp3files = File.join(FTP_PATH, "**", "*.mp3")
 		@files = Dir.glob(mp3files).sort
-		@collections
+		@collections = Collection.all
 	end
 
 	def show
