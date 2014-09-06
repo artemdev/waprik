77c1b9e47f4579bfe3263b82d054b6a71a7444bb
artway <artem.zimovets@gmail.com>
2014-09-06 16:51:42 +0300

    новые жанры

---
+++ app/models/genre_through.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: films_genres_through
+#
+#  genreable_id   :integer
+#  genre_id       :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#  genreable_type :string(255)
+#
+
+class GenreThrough < ActiveRecord::Base
+  # attr_accessible :title, :body
+  self.table_name = "films_genres_through"
+
+  belongs_to :genre, class_name: "FilmGenre"
+  belongs_to :genreable, polymorphic: true
+
+end
