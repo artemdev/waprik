3217aa31d109a0b6357ec764f27d6abc33f63a36
artway <artem.zimovets@gmail.com>
2014-09-09 22:56:03 +0300

    музыка

--- app/controllers/public/music_controller.rb
+++ app/controllers/public/music_controller.rb
@@ -17,7 +17,7 @@ class Public::MusicController < ApplicationController
 
 	def show
 		@track = Mp3File.find_by_permalink(params[:id])
-		redirect_to music_path if @track.nil?
+		redirect_to tracks_path if @track.nil?
 	end
 
 	def download
