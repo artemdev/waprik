aa8e890ec326a5261e812ebc8d5cc9c2b24119d4
artway <artem.zimovets@gmail.com>
2014-11-07 00:45:04 +0200

    постинг фильмов в vk

--- app/workers/serials_worker.rb
+++ app/workers/serials_worker.rb
@@ -4,8 +4,8 @@ class SerialsWorker
 	def perform(id, file_path, quality_id, count)
 		serie = Series.find(id)
 		serie.files.new.convert_to_3gp(file_path, quality_id)
-		# serie.files.new.convert_to_mp4_320(file_path, quality_id)
-		# serie.files.new.convert_to_mp4_640(file_path, quality_id)
+		serie.files.new.convert_to_mp4_320(file_path, quality_id)
+		serie.files.new.convert_to_mp4_640(file_path, quality_id)
 	end
 
 end
