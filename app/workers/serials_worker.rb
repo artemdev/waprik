6b03ba243a743bd0a4ca38a0a822f34909bab50d
artway <artem.zimovets@gmail.com>
2014-11-17 16:50:04 +0200

    исправлен баг с конвертацией

--- app/workers/serials_worker.rb
+++ app/workers/serials_worker.rb
@@ -1,12 +1,14 @@
+require 'video_convertation'
+
 class SerialsWorker
 	include Sidekiq::Worker
 
 	def perform(id, file_path, quality_id, count)
 		serie = Series.find(id)
 		convertation = VideoConvertation.new
-		convertation.to_3gp(serie.files.new, file_path, quality_id)
-		convertation.to_mp4_320(serie.files.new, file_path, quality_id)
-		convertation.to_mp4_640(serie.files.new, file_path, quality_id)
+		convertation.serial_to_3gp(serie.files.new, file_path, quality_id)
+		convertation.serial_to_mp4_320(serie.files.new, file_path, quality_id)
+		convertation.serial_to_mp4_640(serie.files.new, file_path, quality_id)
 	end
 
 end
