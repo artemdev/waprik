a99a682ce7cec603a1432268e47c00a9d0e05f79
artway <artem.zimovets@gmail.com>
2015-04-09 19:12:12 +0300

    новости к фильмам

--- app/controllers/admin/film_genres_controller.rb
+++ app/controllers/admin/film_genres_controller.rb
@@ -26,9 +26,9 @@ class Admin::FilmGenresController < ApplicationController
  		@genre = FilmGenre.find(params[:id])
     content = params[:content] 
     if content && content == "serials"
-      @serials = @genre.serials.paginate(page: params[:page], per_page: 10)
+      @serials = @genre.serials.latest.paginate(page: params[:page], per_page: 10)
     else
-      @films = @genre.films.visible.paginate(page: params[:page], per_page: 10)
+      @films = @genre.films.visible.latest.paginate(page: params[:page], per_page: 10)
     end
   end
 
