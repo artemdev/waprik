f1bca77a0829e4f80a8750023a29ad4b02247370
artway <artem.zimovets@gmail.com>
2015-04-15 14:54:18 +0300

    - default host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery
   
-  def default_url_options
-    if Rails.env.production?
-      {:host => "waprik.org"}
-    else  
-      {}
-    end
-  end
+  # def default_url_options
+  #   if Rails.env.production?
+  #     {:host => "waprik.org"}
+  #   else  
+  #     {}
+  #   end
+  # end
 
   protected
 
