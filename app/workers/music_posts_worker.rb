b21097dc869883eb183fd7489fdb1a264432987c
artway <artem.zimovets@gmail.com>
2015-07-02 11:49:25 +0300

    seo фильмов

--- app/workers/music_posts_worker.rb
+++ app/workers/music_posts_worker.rb
@@ -31,7 +31,7 @@ class MusicPostsWorker
 				# конвертация
 				LameWorker.perform_async(mp3.id)
 				collection.tracks << mp3
-				collection.with_music!
+				collection.with_music! unless collection.with_music
 				count += 1
 			end
 		end
