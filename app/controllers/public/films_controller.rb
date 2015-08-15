fecbd01bddc7a0630e1d81754bc79c648d4cbc5f
artway <artem.zimovets@gmail.com>
2015-08-15 08:56:46 +0300

    плейлисты

--- app/controllers/public/films_controller.rb
+++ app/controllers/public/films_controller.rb
@@ -19,7 +19,7 @@ class Public::FilmsController < ApplicationController
 			@genres = FilmGenre.all
 			@films = []
 		end
-		@collections = Collection.latest.limit(20)
+		@collections = Collection.limit(20)
 		@latest_date = Film.latest.visible.first.created_at.strftime("%d.%m.%y") if Film.latest.visible.first
 		@collections_genres = CollectionGenre.all
 		# @favourite_date = Film.latest.favourite.first.created_at
