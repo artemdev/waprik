7ed07ed2f67ed88b8e9c5975a284caa26cc45401
artway <artem.zimovets@gmail.com>
2015-08-23 22:25:29 +0300

    коллекции

--- app/controllers/public/collections_controller.rb
+++ app/controllers/public/collections_controller.rb
@@ -7,7 +7,7 @@ class Public::CollectionsController < ApplicationController
 
 	def show
 		@collection = Collection.find_by_permalink(params[:id])
-		@tracks = @collection.tracks
+		@tracks = @collection.tracks.page(params[:page]).per_page(10)
 	end
 
 	def join_tracks
